1bf4949a6a87ce0dae1ae627341c8bd3a3aecc3c
macintoshi <dekodumbadze@gmail.com>
2025-03-10 20:06:52 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import {client, connectToDatabase} from "./services/database.js";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get('/', async (req, res) => {
+    async function run() {
+        try {
+            const database = await connectToDatabase();
+        } catch (error) {
+            console.error('Error handling request:', error);
+            res.status(500).json({error: 'Internal server error'});
+        }
+    }
+    run().catch(console.dir);
+    res.json('server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ services/database.js
@@ -0,0 +1,17 @@
+import {MongoClient} from "mongodb";
+
+const uri = `mongodb+srv://dekodumbadze:dekukuna123@clusterdemo.z2nma.mongodb.net/?retryWrites=true&w=majority&appName=Clusterdemo`;
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+        return client.db('cyber_db');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        throw error;
+    }
+}
+
+export {connectToDatabase, client};
