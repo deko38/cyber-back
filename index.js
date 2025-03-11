a9a572632fd692f29335551662723859da2e2446
macintoshi <dekodumbadze@gmail.com>
2025-03-11 22:55:55 +0400

    for navbar,used get for menus and got documents from  menu collection

--- index.js
+++ index.js
@@ -11,18 +11,39 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.get('/', async (req, res) => {
+// app.get('/', async (req, res) => {
+//     async function run() {
+//         try {
+//             const database = await connectToDatabase();
+//         } catch (error) {
+//             console.error('Error handling request:', error);
+//             res.status(500).json({error: 'Internal server error'});
+//         }
+//     }
+//     run().catch(console.dir);
+//     res.json('server is running');
+// });
+
+
+
+app.get('/menus', async (req, res) => {
     async function run() {
         try {
             const database = await connectToDatabase();
+            const menus = database.collection('menus');
+            const cursor = menus.find();
+            const result = await cursor.toArray()
+
+            res.json(result);
         } catch (error) {
             console.error('Error handling request:', error);
             res.status(500).json({error: 'Internal server error'});
         }
     }
     run().catch(console.dir);
-    res.json('server is running');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
